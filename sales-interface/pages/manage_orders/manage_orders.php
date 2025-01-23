31ba932234772b21566e0728afc60379b38e49e4
nibber012 <abellera.cj.bsinfotech@gmail.com>
2025-01-23 19:59:21 +0800

    asd
    asd

--- sales-interface/pages/manage_orders/manage_orders.php
+++ sales-interface/pages/manage_orders/manage_orders.php
@@ -76,7 +76,7 @@
                                     <span>32</span>
                                     <span>
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
-                                            <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/>
+                                            <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm80-160h80v-360h-80v360Zm160 0h80v-360h-80v360Z"/>
                                         </svg>
                                     </span>
                                 </li>
@@ -86,7 +86,27 @@
                                     <span>32</span>
                                     <span>
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
-                                            <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/>
+                                            <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm80-160h80v-360h-80v360Zm160 0h80v-360h-80v360Z"/>
+                                        </svg>
+                                    </span>
+                                </li>
+                                <li class="item">
+                                    <span>PRD-112</span>
+                                    <span>Product GT1</span>
+                                    <span>32</span>
+                                    <span>
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+                                            <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm80-160h80v-360h-80v360Zm160 0h80v-360h-80v360Z"/>
+                                        </svg>
+                                    </span>
+                                </li>
+                                <li class="item">
+                                    <span>PRD-112</span>
+                                    <span>Product GT1</span>
+                                    <span>32</span>
+                                    <span>
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+                                            <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm80-160h80v-360h-80v360Zm160 0h80v-360h-80v360Z"/>
                                         </svg>
                                     </span>
                                 </li>
